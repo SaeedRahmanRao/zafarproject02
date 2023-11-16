4feada91b9310bfa7735f74842b466c3a79a1c07
SaeedRahmanRao <raosaeedrahmankhan@gmail.com>
2023-11-17 00:46:52 +0500

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,105 +1,53 @@
-// ProductDetails.js
+// YourComponent.js
+import React from 'react';
+import './YourComponent.css'; // Import your CSS file for styling
 
-import React, { useState } from 'react';
-import './ProductDetails.css';
-import moonIcon from './moon-icon.png'; // Replace with your moon icon path
-import sunIcon from './sun-icon.png'; // Replace with your sun icon path
-
-const ProductDetails = () => {
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [darkMode, setDarkMode] = useState(false);
-  const [activeButton, setActiveButton] = useState(null);
-
-  const handleCategoryClick = (category) => {
-    setSelectedCategory(category);
-    setActiveButton(category);
-  };
-
-  const handleModeToggle = () => {
-    setDarkMode(!darkMode);
-  };
-
-  const imageUrls = [
-    'https://via.placeholder.com/350/007f00/000000?text=Leaf+Image+1',
-    'https://via.placeholder.com/350/00f800/000000?text=Leaf+Image+2',
-    'https://via.placeholder.com/350/006f00/000000?text=Leaf+Image+3',
-    // More placeholder image URLs if needed
+const YourComponent = () => {
+  const boxColors = [
+    'rgba(128, 128, 128, 0.5)',
+    'rgba(128, 128, 128, 0.5)',
+    'rgba(128, 128, 128, 0.5)',
+    'rgba(128, 128, 128, 0.5)',
+    'rgba(128, 128, 128, 0.5)',
+    'rgba(128, 128, 128, 0.5)',
+    'rgba(128, 128, 128, 0.5)',
+    'rgba(128, 128, 128, 0.5)',
+    'rgba(128, 128, 128, 0.5)',
   ];
 
-  const categoryDetails = {
-    'Cost saving': {
-      description: 'Implementing innovative cost-saving measures has been a cornerstone of our success. Our product provides a comprehensive suite of tools designed to identify, analyze, and optimize expenditures. By leveraging cutting-edge algorithms and predictive analytics, we empower businesses to streamline operations, thereby reducing expenses by at least 30%. Our cost-saving solutions cover a spectrum of areas, from resource allocation to process optimization, ensuring maximal efficiency across the board. By adopting our product, businesses have seen a substantial increase in their bottom line while maintaining operational excellence.',
-      imageUrl: imageUrls[0],
-    },
-    'Space-Efficient': {
-      description: 'Space efficiency is critical in today\'s data-driven world. Our solution revolutionizes data storage, employing state-of-the-art compression algorithms that maximize storage capacity without compromising performance. This next-generation approach allows businesses to store significantly more data within the same physical space. By efficiently utilizing storage resources, our product minimizes hardware requirements and associated costs. Whether dealing with massive databases or cloud storage, our space-efficient solution ensures optimal utilization of available space while enhancing accessibility and retrieval speed.',
-      imageUrl: imageUrls[1],
-    },
-    'Energy Saving': {
-      description: 'Our commitment to energy conservation is at the heart of our product\'s design. By employing innovative power management techniques, we significantly reduce energy consumption by up to 40%. This reduction not only leads to substantial cost savings but also underscores our dedication to environmental sustainability. From intelligent power distribution to optimizing device efficiency, our product ensures minimal energy wastage while maintaining peak performance levels. Join us in our mission to conserve energy and create a greener, more sustainable future.',
-      imageUrl: imageUrls[2],
-    },
-  };
+  const boxTexts = [
+    'Text 1',
+    'Text 2',
+    'Text 3',
+    'Text 4',
+    'Text 5',
+    'Text 6',
+    'Text 7',
+    'Text 8',
+    'Text 9',
+    // Add more text here...
+  ];
 
   return (
-    <div className={darkMode ? 'dark-mode' : 'light-mode'}>
-      <div className="toggle-button" onClick={handleModeToggle}>
+    <div className="container">
+      <div className="background">
+        {/* Your blurred background image */}
+        {/* You can replace 'background-image-url' with your actual background image URL */}
         <img
-          src={darkMode ? sunIcon : moonIcon}
-          alt={darkMode ? 'Light Mode' : 'Dark Mode'}
-          width="24"
-          height="24"
+          src="https://via.placeholder.com/350/006f66/ff0000?text=Leaf+Image+3"
+          alt="Background"
+          className="blur"
         />
       </div>
-      <div className="category-buttons">
-        <button
-          onClick={() => handleCategoryClick('Cost saving')}
-          className={activeButton === 'Cost saving' ? 'specific-button active' : 'specific-button'}
-        >
-          Cost saving
-        </button>
-        {/* Placeholder buttons for other categories */}
-        {/* Replace the text and onClick handlers as needed */}
-        <button
-          onClick={() => handleCategoryClick('Space-Efficient')}
-          className={
-            activeButton === 'Space-Efficient' ? 'specific-button active' : 'specific-button'
-          }
-        >
-          Space-Efficient
-        </button>
-        <button
-          onClick={() => handleCategoryClick('Energy Saving')}
-          className={
-            activeButton === 'Energy Saving' ? 'specific-button active' : 'specific-button'
-          }
-        >
-          Energy Saving
-        </button>
-      </div>
-      <div className="detailed-content">
-        {selectedCategory && (
-          <div>
-            <div>
-              <h2>{selectedCategory}</h2>
-            </div>
-            <p>{categoryDetails[selectedCategory].description}</p>
-            <div>
-              <img src={categoryDetails[selectedCategory].imageUrl} alt={selectedCategory} />
-            </div>
-            <p>{categoryDetails[selectedCategory].description}</p>
-            <p>{categoryDetails[selectedCategory].description}</p>
-            <p>{categoryDetails[selectedCategory].description}</p>
-            <div>
-              <img src={categoryDetails[selectedCategory].imageUrl} alt={selectedCategory} />
-            </div>
-            <p>{categoryDetails[selectedCategory].description}</p>
-            <p>{categoryDetails[selectedCategory].description}</p>
+      <div className="grid">
+        {boxColors.map((color, index) => (
+          <div key={index} className="grid-item" style={{ backgroundColor: color }}>
+            <span className="text">{boxTexts[index]}</span>
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
 };
 
-export default ProductDetails;
+export default YourComponent;
